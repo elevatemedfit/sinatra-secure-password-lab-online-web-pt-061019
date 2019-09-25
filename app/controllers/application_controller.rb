79b20a9106f5a1f4dc57a974cf5f8481553bb384
Chris Junker <elevatemedfit@gmail.com>
2019-09-25 13:01:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,12 +17,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(:username => params[:username], :password => params[:password])
 
-    if user.save
-      redirect '/login'
-    else
+    if params[:username] == nil || params[:password] == niluser.save
       redirect '/failure'
+    else
+      user = User.new(:username => params[:username], :password => params[:password])
+      redirect '/login'
     end
 
   end
